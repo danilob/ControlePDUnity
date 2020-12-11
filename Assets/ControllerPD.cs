243cf1532a42eb313d72c6ddeebd75cb398e2ae4
danilo <daniloborges.cg@gmail.com>
2020-12-11 19:03:15 -0300

    projeto base

---
+++ Assets/ControllerPD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerPD : MonoBehaviour
+{
+    public int angleX = 0;
+    public int angleY = 0;
+    public int angleZ = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ConfigurableJoint joint = this.GetComponent<ConfigurableJoint>(); 
+        joint.targetRotation = Quaternion.Euler(angleX, angleY, angleZ);
+    }
+}
